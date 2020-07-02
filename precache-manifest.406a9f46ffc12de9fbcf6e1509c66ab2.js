9ebf046fdb3414c143a18d4fead1e2e44efe1835
alexgorunescu97 <gorunescualexandru@yahoo.com>
2020-07-02 18:48:53 +0300

    Updates

---
+++ precache-manifest.406a9f46ffc12de9fbcf6e1509c66ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ef15e3bac32eeab71fadafcf94b53d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ea9de73af8a07b270bbd",
+    "url": "/weather-app/static/css/main.7eb9d552.chunk.css"
+  },
+  {
+    "revision": "69e759754c8fc0d92b4d",
+    "url": "/weather-app/static/js/2.ff0d353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.ff0d353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9de73af8a07b270bbd",
+    "url": "/weather-app/static/js/main.17c55887.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "6d30a80a2e6b4c3585daa34301d23ce0",
+    "url": "/weather-app/static/media/background.6d30a80a.jpg"
+  }
+]);
